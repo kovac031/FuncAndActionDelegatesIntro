d3f25ca867e03c56089fb5933c182415621d927d
Ivan <ivan.kovac.email@gmail.com>
2023-12-09 17:49:23 +0100

    init commit

---
+++ DemoLibrary/ShoppingCartModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class ShoppingCartModel
+    {
+        // novo
+
+        // declaring the delegate
+        public delegate void MentionDiscount(decimal subTotal); // DELEGATE ga oboja zeleno, ali je metoda
+
+        // Program.cs ima onu SubTotalAlert(decimal subTotal) koju zelimo passat 
+        // kao parametar za GenerateTotal metodu, i za nju pravimo ovdje delegata
+
+        //------------
+
+        public List<ProductModel> Items { get; set; } = new List<ProductModel>();
+
+        //public decimal GenerateTotal()
+        //public decimal GenerateTotal(MentionDiscount mentionDiscount)
+        public decimal GenerateTotal(
+            MentionDiscount mentionSubtotal,
+            Func<List<ProductModel>, decimal, decimal> calculateDiscountedTotal, // func<in,in,out> naziv
+            Action<string> tellUserWeAreDiscounting // passamo string a ne returna nista, void
+            )
+        {
+            decimal subTotal = Items.Sum(x => x.Price);
+
+            //
+            mentionSubtotal(subTotal); // poziva metodu
+            //
+            tellUserWeAreDiscounting("We are applying your discount.");
+            //
+
+            //if (subTotal > 100)
+            //{
+            //    return subTotal * 0.80M;
+            //}
+            //else if (subTotal > 50)
+            //{
+            //    return subTotal * 0.85M;
+            //}
+            //else if (subTotal > 10)
+            //{
+            //    return subTotal * 0.90M;
+            //}
+            //else
+            //{
+            //    return subTotal;
+            //}
+
+            return calculateDiscountedTotal(Items, subTotal);
+        }
+    }
+}
---
+++ WinFormUI/Dashboard.cs
@@ -0,0 +1,68 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormUI
+{
+    // kao primjer za delegate ali na winformUI primjeru 
+
+    public partial class Dashboard : Form
+    {
+        ShoppingCartModel cart = new ShoppingCartModel();
+
+        public Dashboard()
+        {
+            InitializeComponent();
+            PopulateCartWithDemoData();
+        }
+
+        private void PopulateCartWithDemoData()
+        {
+            cart.Items.Add(new ProductModel { ItemName = "Cereal", Price = 3.63M });
+            cart.Items.Add(new ProductModel { ItemName = "Milk", Price = 2.95M });
+            cart.Items.Add(new ProductModel { ItemName = "Strawberries", Price = 7.51M });
+            cart.Items.Add(new ProductModel { ItemName = "Blueberries", Price = 8.84M });
+        }
+
+        // klikom na gumb Message Box vrti ovo:
+        private void messageBoxDemoButton_Click(object sender, EventArgs e)
+        {
+            decimal total = cart.GenerateTotal(SubTotalAlert, CalculateLeveledDiscount, PrintOutDiscountAlert);
+
+            MessageBox.Show($"The total is {total:C2}");
+        }       
+
+        // isto ali s lambdom za drugi gumb
+        // iako, taj gumb popunjava prazna polja iznosima subTotal i total, a message je ostavljen prazan kao
+        private void textBoxDemoButton_Click(object sender, EventArgs e)
+        {
+            decimal total = cart.GenerateTotal(
+                (subTotal) => subTotalTextBox.Text = $"{subTotal:C2}",
+                (products, subTotal) => subTotal - products.Count(),
+                (message) => { }
+                );
+            totalTextBox.Text = $"{total:C2}";
+        }
+
+        // metode, ali osim sto su parametri za GenerateTotal metodu, ne vidim po cemu su delegati
+        private void SubTotalAlert(decimal subTotal)
+        {
+            MessageBox.Show($"The subtotal is {subTotal:C2}");
+        }
+        private decimal CalculateLeveledDiscount(List<ProductModel> products, decimal subTotal)
+        {
+            return subTotal - products.Count();
+        }
+        private void PrintOutDiscountAlert(string message)
+        {
+            MessageBox.Show(message);
+        }        
+    }
+}
